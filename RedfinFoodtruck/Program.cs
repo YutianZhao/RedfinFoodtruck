01de0b62966467c7fce7274c0813419ca9713807
Yutian Zhao <yutianzhao123@gmail.com>
2020-11-25 19:57:27 -0500

    Fix bug that wont change page.

--- RedfinFoodtruck/Program.cs
+++ RedfinFoodtruck/Program.cs
@@ -28,24 +28,25 @@ namespace RedfinFoodtruck
             int nowOpenCount = nowOpen.Count;
             int pages = nowOpenCount / 10;
             int nowPage = 0;
-            for (int i = 0; i < pages - 1; i++)
+            for (int i = 0; i < pages; i++)
             {
                 for (int j = 0 + 10 * nowPage; j < 10 + 10 * nowPage; j++)
                 {
                     Console.WriteLine(nowOpen[j].Applicant + " ------ " + nowOpen[j].Starttime + " - " + nowOpen[j].Endtime + " ------ " + nowOpen[j].Location);
                 }
-                if (pages > 1)
+                if (pages > 0)
                 {
                     Console.WriteLine("Click any button to contine");
                     Console.ReadLine();
                 }
-                if (pages <= 1 || (nowOpenCount % 10 == 0 && i == pages - 2))
+                if (pages <= 0 || (nowOpenCount % 10 == 0 && i == pages - 1))
                 {
                     Console.WriteLine("List end");
                 }
+                nowPage += 1;
             }
 
-            if (nowOpenCount % 10 != 0 && pages >= 2)
+            if (nowOpenCount % 10 != 0 && pages >= 1)
             {
                 int remained = nowOpenCount % 10;
                 for (int k = 0 + 10 * pages; k < remained + 10 * pages; k++)
